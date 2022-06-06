29e8498482d1ac8d33c1eef218e6b76afe30752f
thisiswaterh2o <79999140+thisiswaterh2o@users.noreply.github.com>
2022-06-05 21:38:17 -0400

    Create 01. Revising the Select Query I.sql

---
+++ 01. Basic Select/01. Revising the Select Query I.sql
@@ -0,0 +1,6 @@
+# PROBLEM: Query all columns for all American cities in the CITY table with populations larger than 100000.
+-----------
+SELECT * FROM CITY
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
+-----------
+# RESULT: PASS
