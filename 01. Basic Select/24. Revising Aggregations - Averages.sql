88c2053c7b7f8fa24fb9fe60a224acfc40ff6e68
thisiswaterh2o <79999140+thisiswaterh2o@users.noreply.github.com>
2022-07-14 10:58:03 -0400

    Create 24. Revising Aggregations - Averages.sql

---
+++ 01. Basic Select/24. Revising Aggregations - Averages.sql
@@ -0,0 +1,6 @@
+# PROBLEM: Query the average population of all cities in CITY where District is California.
+----------
+SELECT AVG(POPULATION) FROM CITY
+WHERE DISTRICT = 'CALIFORNIA';
+----------
+# RESULT: PASS
