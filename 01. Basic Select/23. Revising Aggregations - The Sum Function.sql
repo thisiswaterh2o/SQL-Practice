29b7b3d7b124300316791dd2d7387e838d9b17fb
thisiswaterh2o <79999140+thisiswaterh2o@users.noreply.github.com>
2022-07-14 10:56:52 -0400

    Create 23. Revising Aggregations - The Sum Function.sql

---
+++ 01. Basic Select/23. Revising Aggregations - The Sum Function.sql
@@ -0,0 +1,6 @@
+# PROBLEM: Query the total population of all cities in CITY where District is California.
+----------
+SELECT SUM(POPULATION) FROM CITY
+WHERE DISTRICT = 'CALIFORNIA';
+----------
+# RESULT: PASS
