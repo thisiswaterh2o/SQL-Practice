3441dde152f8b60017519b879661b1648322dd48
thisiswaterh2o <79999140+thisiswaterh2o@users.noreply.github.com>
2022-07-19 23:25:21 -0400

    Create 01. New Companies.sql

---
+++ 02. SQL (Intermediate)/01. New Companies.sql
@@ -0,0 +1,4 @@
+# PROBLEM: Given the table schemas below, write a query to print the 
+# Company_code, founder name, total number of lead managers, total number of senior managers, total number of managers, and total number of employees. 
+# Order your output by ascending company_code.
+----------
